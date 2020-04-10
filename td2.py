57bc7c5ccb8ab3868e4f2da194805485b6ebb538
coulibaly-mouhamed <63076571+coulibaly-mouhamed@users.noreply.github.com>
2020-04-10 20:54:26 +0200

    Add files via upload

---
+++ td2.py
@@ -0,0 +1,31 @@
+class production:
+    def __init__(self,name,year):
+        self.name = name
+        self.year = year
+    def getname(self):
+        return self.name
+    def getyear(self):
+        return self.year
+
+class book:
+    def __init__(self,name,year,edition):
+        self.author = author 
+        self.year = year 
+        self.edition = editon 
+    def getname(self):
+        return self.author
+    def getedition(self):
+        return self.edition
+    
+
+class article:
+    def __init__(self,author,year,review):
+        self.author =author
+        self.year = year
+        self.review =review
+    def getname(self):
+        return self.author
+    def getreview(self):
+        return self.review
+
+
