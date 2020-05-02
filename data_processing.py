478148a8a7dab37480db8d11d565bf919e6241d5
coulibaly-mouhamed <63076571+coulibaly-mouhamed@users.noreply.github.com>
2020-05-02 20:01:02 +0200

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -11,13 +11,32 @@ import pandas as pd
 #Our N_trusted list 
 N_trusted=['nbcnews.com','in.reuters.com','bbc.co.uk','theguardian.com','reuters.com','edition.cnn.com','cbc.com','nationalpost.com','independent.co.uk','cnn.com','uk.reuters.com','time.com','washingtonexaminer.com','news.sky.com','newsweek.com','thesun.co.uk','washingtonpost.com','france24.com','vox.com','coronavirusin.world','dutchnews.nl','nytimes.com','twitter.com','youtube.com','metro.co.uk','bloomberg.com','google.com','japantimes.co.jp','foxnews.com','financialnews.com']
 
-def data_scarping(subreddit):
+#We try to define a model to know how many words match with ours tittle
+#We don't consider synonyms here 
+
+def test(char,char_list):
+    n = len(char)
+    j = len(char)
+    #It's an allowance to 
+    threshold = 0.9*n
+    i=0
+    for words in char:
+        if words in char_list:
+            i +=1
+    if i >= threshold:
+        return True 
+    elif i == j:
+        return True 
+    return False
+
+
+def data_scarping(subreddit,keywords):
     api = PushshiftAPI()
 
     # Create list of data
     scrape_list = list(api.search_submissions(subreddit=subreddit,
                                 filter=['title', 'subreddit', 'num_comments', 'author', 'subreddit_subscribers', 'score', 'domain', 'created_utc'],
-                                limit=10000))
+                                limit=9000))
 
     #Filter list to only show Subreddit titles and Subreddit category 
     clean_scrape_lst = []
@@ -31,17 +50,19 @@ def data_scarping(subreddit):
         scrape_dict['score'] = scrape_list[i][4]
         scrape_dict['timestamp'] = scrape_list[i][1]
         #We just keep news from N_trusted sources
-        if (scrape_dict['domain'] in N_trusted):
-            clean_scrape_lst.append(scrape_dict)
+        if scrape_dict['domain'] in N_trusted:
+            if test(keywords,scrape_dict['title']):
+                clean_scrape_lst.append(scrape_dict)
 
     # Return list of data
     return clean_scrape_lst
 
 
-def data_base():
+def data_base(keywords):
+    words = keywords.split()
     #Here we create a data Frame
     # worldnews is the greates news center in Reddit with millions of user that's why we choose it for a start
-    df = pd.DataFrame(data_scarping('worldnews'))
+    df = pd.DataFrame(data_scarping('worldnews',words))
     #Save data in csv format
     df.to_csv('./database.csv')
     print(f'df shape:{df.shape}')
