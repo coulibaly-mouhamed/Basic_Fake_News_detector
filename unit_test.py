fa66bed298b395f30b7969679d24fd347196bb28
Pourouchottamin-Shamin <63126995+Pourouchottamin-Shamin@users.noreply.github.com>
2020-05-13 16:03:05 +0200

    Create unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from Database import Article, NoletterError, NotaLinkError, NotaTitleError
+
+
+class ScoreGoodOutput(unittest.TestCase):
+def test_percentage(self):
+    """we should always return a score between 0 to 100"""
+    article =https://www.bbc.com/news/world-latin-america-52644339
+    self.assertTrue(0=<Score(article)=<100)
+
+
+class ScoreBadInput(unittest.TestCase):
+
+    def test_number(self):
+        """score should fail with numeral input"""
+        article = 7589
+        self.assertRaises(NoletterError, Score(article))
+
+    def test_wronglink(self):
+        """score should fail with a link that doesn't work"""
+         article = wwh.google.com
+        self.assertRaises(NotaLinkError, Score(article))
+    def test_wrongTitle(self):
+        """score should fail with a title that doesn't correspond to any article"""
+         article = Why qh WHY
+        self.assertRaises(NotaTitleError, Score(article))
+
+if __name__ == "__main__":
+    unittest.main()
