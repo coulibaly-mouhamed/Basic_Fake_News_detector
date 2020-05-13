f5e4f35ba2c1a49b42dab1a47f73bf9c0750e952
Pourouchottamin-Shamin <63126995+Pourouchottamin-Shamin@users.noreply.github.com>
2020-05-13 16:03:52 +0200

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,18 @@
+class NoletterError(ValueError):
+    pass
+
+class NotaLinkError(ValueError):
+    pass
+
+class NotaTitleError(ValueError):
+    pass
+
+
+
+class Errors: pass
+
+
+class InvalidSyntax(Errors): pass
+
+
+class Article:
