9f55b02dfbafd54f8c78abb8694a64c04d4c044a
Pourouchottamin-Shamin <63126995+Pourouchottamin-Shamin@users.noreply.github.com>
2020-05-20 19:42:55 +0200

    Update letter_to_number.py

--- letter_to_number.py
+++ letter_to_number.py
@@ -12,7 +12,7 @@ def letter_to_number(text):
       new_text=[]
       list = [ ,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,.,!,?,-,@,"]
       for i in range(n):
-           if text[i]==,:
+           if text[i]==',':
                   new_text.append(,)
             else :
                   s=oudansliste(list,text[i])
@@ -26,7 +26,7 @@ def number_to_letter(numbers):
       list = [ ,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,.,!,?,-,@,"]
       for i in range(n):
             numero=0
-            if numbers[i]!=-1:
+            if numbers[i]!='-1':
                  numero=numbers[i]
             text.append(list[numero])
       return text
