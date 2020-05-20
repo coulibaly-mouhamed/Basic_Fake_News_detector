b1845aa54d32383e4f2d8f0d5a4bb7d6acbf6ed9
Pourouchottamin-Shamin <63126995+Pourouchottamin-Shamin@users.noreply.github.com>
2020-05-20 21:53:21 +0200

    Update letter_to_number.py

--- letter_to_number.py
+++ letter_to_number.py
@@ -6,18 +6,38 @@ def oudansliste(list,caractere):
          
 
 
+def ajouterchiffre(nombreinit,chiffre2):
+            nombre=10*nombreinit
+            nombreinit=nombre+chiffre2
+      
 
 def letter_to_number(text):
       n=len(text)
-      new_text=[]
+      nombre
       list = [ ,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,.,!,?,-,@,"]
       for i in range(n):
            if text[i]==',':
-                  new_text.append(,)
-            else :
-                  s=oudansliste(list,text[i])
-                 new_text.append(s)
-       return new_text
+                  ajouterchiffre(nombre,0)  #we put a 0 as a separator of LETTERS
+                  ajouterchiffre(nombre,36) # we represent ',' by 36
+            elif (oudansliste(list,text[i])=-1):  #we check if its a letter or a character of pronounciation
+                        s=2+oudansliste(list,text[i]) # we add 2 because 1 and 0 are taken as separators
+                         ajouterchiffre(nombre,0) #we put a 0 as a separator of LETTERS
+                        if s>9:
+                                 ajouterchiffre(nombre,s//10)
+                                   ajouterchiffre(nombre,s%10)
+                        else:
+                                   ajouterchiffre(nombre,s)
+            else: # so we know we are dealing with a number here
+                     s=2+oudansliste(list,text[i]) # we add 2 because 1 and 0 are taken as separators
+                         ajouterchiffre(nombre,1) #we put a 1 as a separator of NUMBERS
+                        if s>9:
+                                 ajouterchiffre(nombre,s//10)
+                                   ajouterchiffre(nombre,s%10)
+                        else:
+                                   ajouterchiffre(nombre,s)
+           
+              
+       return nombre
        
        
 def number_to_letter(numbers):
